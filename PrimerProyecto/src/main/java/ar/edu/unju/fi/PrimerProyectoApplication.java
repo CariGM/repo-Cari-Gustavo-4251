1b5a32533809d5b8d805716fc131844c8a09d6f1
PC <PC@DESKTOP-GFNM3GR>
2022-04-09 00:34:21 -0300

    primer commit

---
+++ PrimerProyecto/src/main/java/ar/edu/unju/fi/PrimerProyectoApplication.java
@@ -0,0 +1,21 @@
+package ar.edu.unju.fi;
+
+import org.springframework.boot.SpringApplication;
+
+public class PrimerProyectoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(PrimerProyectoApplication.class, args);
+		
+		//Calculadora unaCalculadora = new Calculadora();
+		
+		double valor = 2/3;
+		System.out.println(valor);
+
+		boolean resEvaluacion = (5 <= 5) && (4 >= 3);
+		System.out.println(resEvaluacion);
+		
+	}
+
+}
