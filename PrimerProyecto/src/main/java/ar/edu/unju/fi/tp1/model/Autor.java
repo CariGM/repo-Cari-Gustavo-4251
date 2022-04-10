5c1bf662c83e017732225aab7300709509ae4cd6
PC <PC@DESKTOP-GFNM3GR>
2022-04-10 18:04:19 -0300

    Agregue variables de datos y accesores.

--- PrimerProyecto/src/main/java/ar/edu/unju/fi/tp1/model/Autor.java
+++ PrimerProyecto/src/main/java/ar/edu/unju/fi/tp1/model/Autor.java
@@ -2,9 +2,29 @@ package ar.edu.unju.fi.tp1.model;
 
 public class Autor {
 
+
+	private String Nombre;
+	private int edad;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 	}
 
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	
 }
